6ae4f064ea867f8898512b6ed230e027aa581aa1
Marcelino <marceometry@gmail.com>
2021-03-02 09:06:15 -0300

    change color in page icons

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,21 +1,45 @@
-import css from '../css/components/sideBar.module.css'
+import { useState } from "react";
+import css from "../css/components/sideBar.module.css";
 
 export function SideBar() {
-    return (
-        <aside id={css.aside}>
-            <img src="logo.svg" alt=""/>
+  const [home, setHome] = useState(true);
+  const [ranking, setRanking] = useState(false);
 
-            <nav>
-                <div>
-                    <div className={`${css.page} ${css.active}`}></div>
-                    <img className={css.active} src="icons/home.svg" alt="Home"/>
-                </div>
+  function openHome() {
+    setHome(true)
+    setRanking(false)
+  }
 
-                <div>
-                    <div className={`${css.page}`}></div>
-                    <img src="icons/ranking.svg" alt="Ranking"/>
-                </div>
-            </nav>
-        </aside>
-    )
+  function openRanking() {
+    setHome(false)
+    setRanking(true)
+  }
+
+  return (
+    <aside id={css.aside}>
+      <img src="logo.svg" alt="Logo" />
+
+      <nav>
+        <div>
+          <div className={`${css.page} ${home ? css.active : ''}`}></div>
+
+          { home ? (
+            <img src="icons/home-active.svg" alt="Home" />
+          ) : (
+            <img onClick={openHome} src="icons/home.svg" alt="Home" />
+          )}
+        </div>
+
+        <div>
+          <div className={`${css.page} ${ranking ? css.active : ''}`}></div>
+
+          { ranking ? (
+            <img src="icons/ranking-active.svg" alt="Ranking" />
+          ) : (
+            <img onClick={openRanking} src="icons/ranking.svg" alt="Ranking" />
+          )}
+        </div>
+      </nav>
+    </aside>
+  );
 }
