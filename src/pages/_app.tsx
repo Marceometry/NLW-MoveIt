e813c6b2f3b3887784fba2ea301adfcffaacb533
Marcelino <marceometry@gmail.com>
2021-05-11 09:17:23 -0300

    add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import { Provider } from 'next-auth/client'
+import Router from 'next/router'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import '../css/global.css'
 import '../css/animations.css'
+
+Nprogress.configure({
+  showSpinner: false,
+  trickleSpeed: 300
+})
+
+Router.events.on('routeChangeStart', () => {
+  Nprogress.start()
+})
+
+Router.events.on('routeChangeComplete', () => {
+  Nprogress.done()
+})
+
+Router.events.on('routeChangeError', () => {
+  Nprogress.done()
+})
 
 export default function MyApp({ Component, pageProps }) {
   return (
